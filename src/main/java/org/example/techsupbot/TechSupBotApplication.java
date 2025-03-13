1bb3ad50bd3d2dbf67e77f3788bd1c56eee689c8
olegz <OLEGZAOSTROVTSEV19@yandex.ru>
2025-03-13 23:23:09 +0300

    first commit

---
+++ src/main/java/org/example/techsupbot/TechSupBotApplication.java
@@ -0,0 +1,13 @@
+package org.example.techsupbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechSupBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechSupBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/techsupbot/TechSupBotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.techsupbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechSupBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
