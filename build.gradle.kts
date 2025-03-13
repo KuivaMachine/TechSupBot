359f0abec53c9aeebfc83792ab0fb3f3902d38a7
olegz <OLEGZAOSTROVTSEV19@yandex.ru>
2025-03-13 23:55:10 +0300

    first steps

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,12 +24,12 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+
     implementation("org.springframework.boot:spring-boot-starter-web")
     compileOnly("org.projectlombok:lombok")
     runtimeOnly("org.postgresql:postgresql")
     annotationProcessor("org.projectlombok:lombok")
+    implementation("org.telegram:telegrambots:6.9.7.1")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
