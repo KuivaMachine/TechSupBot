4535a00635bc5fa995da7045b9efd3b870c775c9
olegz <OLEGZAOSTROVTSEV19@yandex.ru>
2025-04-07 18:09:09 +0300

    NEW

--- src/main/java/org/example/techsupbot/bot/TechSupBot.java
+++ src/main/java/org/example/techsupbot/bot/TechSupBot.java
@@ -1,32 +1,14 @@
 package org.example.techsupbot.bot;
 
 import lombok.extern.log4j.Log4j2;
-import org.example.techsupbot.DTO.Client;
 import org.example.techsupbot.DTO.ClientService;
-import org.example.techsupbot.data.ButtonLabels;
-import org.example.techsupbot.data.ClientStatus;
 import org.example.techsupbot.redis.RedisService;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.bots.TelegramWebhookBot;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
-import org.telegram.telegrambots.meta.api.methods.send.SendMediaGroup;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.methods.updates.SetWebhook;
-import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessage;
-import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.media.InputMedia;
-import org.telegram.telegrambots.meta.api.objects.media.InputMediaPhoto;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardRemove;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
-
-import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
 
 
 @Log4j2
@@ -66,12 +48,12 @@ public class TechSupBot extends TelegramWebhookBot {
 
     @Override
     public BotApiMethod<?> onWebhookUpdateReceived(Update update) {
-        return null;
+        return controller.receiveUpdate(update);
     }
 
     @Override
     public String getBotPath() {
-        return "";
+        return "/bot_tech_sup/update";
     }
 }
 
